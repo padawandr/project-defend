807ebd53257560ece6113f385e43e174141041cb
Amanda Nobre <amandapnobre@gmail.com>
2020-09-30 17:19:36 -0300

    Adding Slider
    Adding slider and limiting it.

---
+++ Project Defend/Assets/Scripts/Slider.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slider : MonoBehaviour
+{
+
+    public float velocidadeSlider;
+    [Header ("Configuração Limite")]
+    public float limiteYMaximo;
+    public float limiteYMinimo;
+    public float limiteXMaximo;
+    public float limiteXMinimo;
+    private Rigidbody2D rBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        rBody.velocity = new Vector2(horizontal * velocidadeSlider, vertical * velocidadeSlider);
+
+        //Ajusta o limite Y
+        if(transform.position.y > limiteYMaximo) 
+        {
+            transform.position = new Vector3(transform.position.x , limiteYMaximo , 0);
+        }
+        else if(transform.position.y < limiteYMinimo) 
+        {
+            transform.position = new Vector3(transform.position.x , limiteYMinimo , 0);
+        }
+
+        //Ajusta o limite X
+        if(transform.position.x > limiteXMaximo) 
+        {
+            transform.position = new Vector3(limiteXMaximo , transform.position.y , 0);
+        }
+        else if(transform.position.x < limiteXMinimo) 
+        {
+            transform.position = new Vector3(limiteXMinimo , transform.position.y , 0);
+        }
+    }
+}
