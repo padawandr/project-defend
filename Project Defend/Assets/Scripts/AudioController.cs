1127ef98f5638a44a3ac5ce229b72fcc04bd74bf
Amanda Nobre <amandapnobre@gmail.com>
2020-10-07 17:19:19 -0300

    Adding the AudioController

---
+++ Project Defend/Assets/Scripts/AudioController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public AudioSource audioSourceMusicaDeFundo;
+    public AudioClip[] musicasDeFundo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Escolhe aleatóriamente a música de fundo em uma array
+        int IndexDaMusicaDeFundo = Random.Range(0, musicasDeFundo.Length);
+        //Toca a música
+        AudioClip musicaGamePlay = musicasDeFundo[IndexDaMusicaDeFundo];
+        audioSourceMusicaDeFundo.clip = musicaGamePlay;
+        audioSourceMusicaDeFundo.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
