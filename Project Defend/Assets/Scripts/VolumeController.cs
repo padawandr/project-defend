fb6fdb4ba15641f875634d7a9c985cb0ba16b3c5
Amanda Nobre <amandapnobre@gmail.com>
2020-10-22 21:52:38 -0300

    Adding the Volume Config. screen

---
+++ Project Defend/Assets/Scripts/VolumeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeController : MonoBehaviour
+{
+    //controla o volume geral do jogo
+
+    float volumeMaster;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void VolumeMaster(float volume) 
+    {
+        volumeMaster = volume;
+        AudioListener.volume = volumeMaster;
+    }
+}
