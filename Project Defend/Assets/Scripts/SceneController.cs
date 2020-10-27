672a53cf12157fe0d25187188780bb104233fc38
João Vitor Lopes de Farias <jvlopesfarias@gmail.com>
2020-10-27 10:56:26 -0300

    Update menu scene

--- Project Defend/Assets/Scripts/SceneController.cs
+++ Project Defend/Assets/Scripts/SceneController.cs
@@ -5,8 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    public GameObject ConfigMenu;
+
     public void changeScene(string nextScene)
     {
         SceneManager.LoadScene(nextScene);
+    }
+
+    public void pokedexScene(string pokedex)
+    {
+        SceneManager.LoadScene(pokedex);
+    }
+
+    public void BackMenu()
+    {
+        ConfigMenu.SetActive(false);
+ 
+    }
+
+    public void Settngs()
+    {
+        ConfigMenu.SetActive(true);
     }
 }
