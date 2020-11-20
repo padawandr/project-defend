d0e9c98f31bc1a98caae1f3c54fc73f8a40b44f1
Eruel6 <arthur_taylor2001@hotmail.com>
2020-11-20 14:39:24 -0300

    Stopped the movement of the worm
    Parando a lagarta em uma parte especifica da animação

--- Project Defend/Assets/Scripts/Worm.cs
+++ Project Defend/Assets/Scripts/Worm.cs
@@ -18,7 +18,8 @@ public class Worm : MonoBehaviour
     private SpriteRenderer srend;
     private Collider2D col;
     private bool podeAndar;
-    private bool estaMorto; 
+    private bool estaMorto;
+    private int caso;
 
     private void Start()
     {
@@ -33,11 +34,11 @@ public class Worm : MonoBehaviour
         vida.SetMaxHealth(maxHealth);
 
         switch (transform.position.x) {
-            case -4: velocidade.x = .32F; break;
-            case -2: velocidade.x = .15F; break;
-            case 0: velocidade.x = 0; break;
-            case 2: velocidade.x = -.15F; break;
-            case 4: velocidade.x = -.32F; break;
+            case -4: velocidade.x = .32F; caso = 1 ; break;
+            case -2: velocidade.x = .15F; caso = 2; break;
+            case 0: velocidade.x = 0; caso = 3;  break;
+            case 2: velocidade.x = -.15F; caso = 4; break;
+            case 4: velocidade.x = -.32F; caso = 5;  break;
         }
     }
 
@@ -53,7 +54,8 @@ public class Worm : MonoBehaviour
     private void FixedUpdate()
     {
         if (podeAndar) {
-            rbody.MovePosition(rbody.position + velocidade * Time.fixedDeltaTime);
+
+                    rbody.MovePosition(rbody.position + velocidade * Time.fixedDeltaTime);
         }
     }
 
@@ -88,4 +90,22 @@ public class Worm : MonoBehaviour
         vida.SetHealht(currentHealth);
     }
 
+    public void Stop()
+    {
+        velocidade.x = 0;
+        velocidade.y = 0;
+    }
+
+    public void Walk()
+    {
+        switch (caso)
+        {
+            case 1: velocidade.x = .32F; break;
+            case 2: velocidade.x = .15F; break;
+            case 3: velocidade.x = 0; break;
+            case 4: velocidade.x = -.15F; break;
+            case 5: velocidade.x = -.32F; break;
+        }
+        velocidade.y = -1;
+    }
 }
